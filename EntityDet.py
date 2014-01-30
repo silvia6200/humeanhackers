76fd7e1376937f390db8a27ee95a82a5c00eefef
Vanessa Hanschke <emmawbv@hotmail.com>
2014-01-30 17:47:23 +0100

    small code changes in entity det

--- EntityDet.py
+++ EntityDet.py
@@ -1,19 +1,22 @@
 import nltk, re, pprint
 # Splits ready Dataset 
 
-def detectEnt(self):
-		
-	#entity detection - chunking
-
+def detectEnt(sentences):
+	
 	grammar = r"""
 		NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
 		PP: {<IN><NP>}               # Chunk prepositions followed by NP
-		VB: {<VB.>+}				# Chunk sequence of verbs
-		VP: {<VB><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
+		VB: {<VB.>+}				 # Chunk sequence of verbs
+		VP: {<VB><NP|PP|CLAUSE>+$}   # Chunk verbs and their arguments
 		CLAUSE: {<NP><VP>}           # Chunk NP, VP
 		"""
 	cp = nltk.RegexpParser(grammar, loop=2)
-		
+
+	for sent in sentences
+	#entity detection - chunking
+		sentnamed = nltk.ne_chunk(sent)
+		sentchunk = cp.parse(sentnamed)
+				
 	# create a dictionary of chunks
 	c = {}
 	C = 0
