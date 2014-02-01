edd42c10525999b248579e4515495fa9015f2167
Silvia Colombo <lucy6200@gmail.com>
2014-02-01 19:10:32 +0100

    adding two nodes and a relationship to the graph

--- NeoCreate.py
+++ NeoCreate.py
@@ -9,38 +9,15 @@ def addtodb(rel):
 	relationship = rel['filler']
 
 	#cleaning the filler of everything after the slash
-	if(relationship.find('/')>0)
+	if(relationship.find('/')>0):
 		relationship = relationship[0:relationship.find('/')]
 
 	with db.transaction:
 		rightnode = db.node(name=rel['subjsym'])
 		leftnode = db.node(name=rel['objectsym'])
 
+		leftnode.relationships.create(relationship, rightnode)
 
-		
+	print "Created nodes " + leftnode + " and " + rightnode + " with relationship " + relationship +"! \n"
 
-
-
-
-
-# All write operations happen in a transaction
-# with db.transaction:
-#     firstNode = db.node(name='Hello')
-#     secondNode = db.node(name='world!')
- 
-#     # Create a relationship with type 'knows'
-#     relationship = firstNode.knows(secondNode, name='graphy')
- 
-# Read operations can happen anywhere
-# message = ' '.join([firstNode['name'], relationship['name'], secondNode['name']])
- 
-# print message
- 
-# Delete the data
-# with db.transaction:
-#     firstNode.knows.single.delete()
-#     firstNode.delete()
-#     secondNode.delete()
- 
-# Always shut down your database when your application exits
-# db.shutdown()
+	db.shutdown()
