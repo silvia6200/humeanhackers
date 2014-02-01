d525b2ff14e96616d5225e3029f609c0a064c66b
Silvia Colombo <lucy6200@gmail.com>
2014-02-01 18:05:51 +0100

    adds the nodes to the db but not the relations

--- NeoCreate.py
+++ NeoCreate.py
@@ -3,24 +3,44 @@ from neo4j import GraphDatabase
 # Create a database
 db = GraphDatabase(reldatabase)
  
+def addtodb(rel):
+#	rel['subjsym'], rel['objectsym'], rel['filler'] 
+	
+	relationship = rel['filler']
+
+	#cleaning the filler of everything after the slash
+	if(relationship.find('/')>0)
+		relationship = relationship[0:relationship.find('/')]
+
+	with db.transaction:
+		rightnode = db.node(name=rel['subjsym'])
+		leftnode = db.node(name=rel['objectsym'])
+
+
+		
+
+
+
+
+
 # All write operations happen in a transaction
-with db.transaction:
-    firstNode = db.node(name='Hello')
-    secondNode = db.node(name='world!')
+# with db.transaction:
+#     firstNode = db.node(name='Hello')
+#     secondNode = db.node(name='world!')
  
-    # Create a relationship with type 'knows'
-    relationship = firstNode.knows(secondNode, name='graphy')
+#     # Create a relationship with type 'knows'
+#     relationship = firstNode.knows(secondNode, name='graphy')
  
 # Read operations can happen anywhere
-message = ' '.join([firstNode['name'], relationship['name'], secondNode['name']])
+# message = ' '.join([firstNode['name'], relationship['name'], secondNode['name']])
  
-print message
+# print message
  
 # Delete the data
-with db.transaction:
-    firstNode.knows.single.delete()
-    firstNode.delete()
-    secondNode.delete()
+# with db.transaction:
+#     firstNode.knows.single.delete()
+#     firstNode.delete()
+#     secondNode.delete()
  
 # Always shut down your database when your application exits
-db.shutdown()
+# db.shutdown()
