d283705e97b1c365ba58c8f75741ceda9199cb3d
Silvia Colombo <lucy6200@gmail.com>
2014-02-09 17:54:13 +0100

    established that creating the nodes works

--- NeoQuery.py
+++ NeoQuery.py
@@ -1,12 +1,17 @@
 from neo4j import GraphDatabase
  
 # Create a database
-db = GraphDatabase('reldatabase')
+db = GraphDatabase("reldatabase")
 
 def showAllNodes():
+	number_of_nodes = len(db.nodes)
+	print "This db has " + number_of_nodes +"nodes" 
 	
-	for node in db.nodes:
-		print node + "\n"
+	if(number_of_nodes>0):
+		for node in db.nodes:
+			print node + "\n"
+	else: 
+		print "no nodes"
 
 
 
